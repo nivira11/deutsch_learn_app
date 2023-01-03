30a1b6c346dea3019640cc0e769c0816bce4708b
YevhenOrlov <jorlov48@gmail.com>
2023-01-03 14:29:23 +0100

    v2

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import random
 from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.uix.button import Button
+from kivy.uix.screenmanager import Screen
 
 Builder.load_string("""
 
